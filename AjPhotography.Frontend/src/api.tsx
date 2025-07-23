60f591c97156ef05f16dbff8e79e1be63d60786e
Anders Sven Axel Johansson <anderssvenaxelj@gmail.com>
2025-07-23 22:17:42 +0200

    Create api file

---
+++ AjPhotography.Frontend/src/api.tsx
@@ -0,0 +1,3 @@
+const API_BASE_URL = 'https://localhost:5000/api';
+
+ export async function fetch
---
+++ AjPhotography.Frontend/src/pages/Home.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import './Home.css';
+
+const images = [
+    '/1.jpg',
+    '/2.jpg',
+    '/3.jpg',
+    '/4.jpg',
+    '/5.jpg',
+    '/6.jpg',
+    '/7.jpg',
+    '/8.jpg',
+]
+
+function Home() {
+    const [centerIndex, setCenterIndex] = useState<number>(2)
+    
+    const prev = () => {
+        setCenterIndex((i: number) => (i === 0 ? images.length - 1 : i - 1))
+    }
+    
+    const next = () => {
+        setCenterIndex((i: number) => (i === images.length - 1 ? 0 : i + 1))
+    }
+
+    return (
+        <>
+            <div className="images-container">
+                <div className="category-container">
+                    <p>PORTRÄTT</p>
+                        {images.map((_, i) => {
+                        const image = images[(centerIndex + i) % images.length];
+                        return (
+                            <img
+                                key={i}
+                                className={`image ${i === 2 ? 'center' : ''}`}
+                                src={image}
+                                alt={`${i + 1}`}
+                            />
+                        );
+                    })}                   
+                </div>
+                <div className="category-container">
+                    <p>NATUR</p>
+                </div>                
+                <div className="category-container">
+                    <p>BRÖLLOP</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
